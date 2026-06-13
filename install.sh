c3a75dee49dc8fb7b9a76a66ca6032b613fb41e3
NETHEAD001-vpsscript <nethead001nethead001@gmail.com>
2026-06-13 15:28:21 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,314 @@
+#!/bin/bash
+
+|════════════════════════════════════════════════════════════════════════════════════════════════════════════════|
+• Autoscript AIO Lite Menu By cyber Project |
+• cyber Project Developer @nethead001 | @nethead001 | https://t.me/WORLDWILDINTERNETTECH |
+• Copyright 2024 18 cyber Decode [ ] | [ @nethead001 ] | [ jamaice ] |
+|════════════════════════════════════════════════════════════════════════════════════════════════════════════════|
+
+link="https://raw.githubusercontent.com/Nethead001/vpsscript/Nethead001/main/install.sh"
+
+clear
+#echo "Port 22 Port 3303 #Port 53" >> /etc/ssh/sshd_config
+#systemctl stop systemd-resolved
+#systemctl restart ssh
+#systemctl restart sshd
+#systemctl start systemd-resolved
+clear
+#Bikin File
+clear
+mkdir -p /etc/slowdns
+mkdir -p /etc/xray
+mkdir -p /etc/funny
+mkdir -p /var
+mkdir -p /var/lib
+mkdir -p /var/log
+mkdir -p /var/log/xray
+touch /var/log/xray/access.log
+chmod +x /var/log/xray/*
+clear
+#domain
+read -rp "Enter Sub Domain: " domain
+#read -p "Nameserver: " nsdomain
+#read -p "Api Bot Token: " api
+#read -p "Admin ID Telegram: " id
+#echo "latex
+api" &gt; /etc/funny/.keybot #echo "
+
+id" > /etc/funny/.chatid
+#echo " domain" > /etc/xray/domain
+#Key
+uuid= {uuid}" > /etc/xray/.key
+chmod +x /etc/xray/.key
+api773=$`(cat /etc/xray/.key)
+#email
+#read -rp "Masukkan Email anda: " email
+#Preparation
+clear
+cd; apt-get update;
+#Remove unused Module
+apt-get -y --purge remove samba*;
+apt-get -y --purge remove apache2*;
+apt-get -y --purge remove sendmail*;
+apt-get -y --purge remove bind9*;
+apt-get -y --purge remove apache2*
+apt remove apache2 -y
+apt autoremove -y
+clear
+#install toolkit
+apt-get install libio-socket-inet6-perl libsocket6-perl libcrypt-ssleay-perl libnet-libidn-perl perl libio-socket-ssl-perl libwww-perl libpcre3 libpcre3-dev zlib1g-dev dbus iftop zip unzip wget net-tools curl nano sed screen gnupg gnupg1 bc apt-transport-https build-essential dirmngr dnsutils sudo at htop iptables bsdmainutils cron lsof lnav -y
+#Install tools
+apt install binutils -y
+apt install socat -y
+apt install lolcat -y
+apt install ruby -y
+gem install lolcat
+apt install lolcat -y
+apt install wget curl -y
+apt install htop -y
+apt install speedtest-cli -y
+apt install cron -y
+apt install figlet -y
+apt install zip unzip -y
+apt install jq -y
+apt install certbot -y
+apt install python2 -y
+apt install python3 -y
+apt install screen -y
+apt install haproxy -y
+apt install at -y
+Information
+curl ipinfo.io/org > /root/.isp
+curl ipinfo.io/city > /root/.city
+curl ipinfo.io/region > /root/.region
+// Menginstall Dropbear
+apt install dropbear -y
+rm /etc/default/dropbear
+rm /etc/issue.net
+cat> /etc/issue.net << END
+</strong> <p style="text-align:center"><b> <br><font color="#00FFE2"<br>┏━━━━━━━━━━━━━━━┓<br> Nethead001 Tricks<br>┗━━━━━━━━━━━━━━━┛<br></font><br><font color="#00FF00"></strong> <p style="text-align:center"><b> <br><font color="#00FFE2">क═══════क⊹⊱✫⊰⊹क═══════क<b></> T̺͆O̺͆ M̺͆Y̺͆ S̺͆E̺͆R̺͆V̺͆E̺͆R̺͆ V͇̿I͇̿P͇̿ ] ★ </b></font><br><font color="#FFF00">Nethead001 Tricks</font><br> <font color="#FF00FF">❖Ƭʜᴇ No DDOS</font><br> <font color="#FF0000">❖get unlimited eSIM</font><br> <font color="#FFB1C2">❖all method </font><b <font color="#00FF00">❖free esim</font><br> <font color="#00FF00">➳ᴹᴿ᭄ Free net : https://t.me/Nethead001trick </font><br> <font color="#00FFE2">क═══════क⊹⊱✫⊰⊹क═══════क</font><br></font><br><font color="FFFF00">❖JOIN My Telegram => https://t.me/Nethead001trick</font><br>
+END
+clear
+cat> /etc/default/dropbear << END
+disabled because OpenSSH is installed
+change to NO_START=0 to enable Dropbear
+NO_START=0
+the TCP port that Dropbear listens on
+DROPBEAR_PORT=111
+any additional arguments for Dropbear
+DROPBEAR_EXTRA_ARGS="-p 109 -p 69 "
+specify an optional banner file containing a message to be
+sent to clients before they connect, such as "/etc/issue.net"
+DROPBEAR_BANNER="/etc/issue.net"
+RSA hostkey file (default: /etc/dropbear/dropbear_rsa_host_key)
+#DROPBEAR_RSAKEY="/etc/dropbear/dropbear_rsa_host_key"
+DSS hostkey file (default: /etc/dropbear/dropbear_dss_host_key)
+#DROPBEAR_DSSKEY="/etc/dropbear/dropbear_dss_host_key"
+ECDSA hostkey file (default: /etc/dropbear/dropbear_ecdsa_host_key)
+#DROPBEAR_ECDSAKEY="/etc/dropbear/dropbear_ecdsa_host_key"
+Receive window size - this is a tradeoff between memory and
+network performance
+DROPBEAR_RECEIVE_WINDOW=65536
+END
+echo "/bin/false" >> /etc/shells
+echo "/usr/sbin/nologin" >> /etc/shells
+dd=$(ps aux | grep dropbear | awk '{print $2}')
+kill `$dd
+clear
+systemctl daemon-reload
+/etc/init.d/dropbear restart
+clear
+#Mengintall Xray
+cd /etc/xray
+wget -q -O config.json "https://raw.githubusercontent.com/Nethead001/sslh/main/config.json"
+#wget -q -O http.json "https://raw.githubusercontent.com/Nethead001/sslh/main/http.json"
+#wget -q -O split.json "https://raw.githubusercontent.com/Nethead001/sslh/main/split.json"
+chmod +x /etc/xray/*
+Other
+cd /usr/bin
+wget -q -O m.zip "https://raw.githubusercontent.com/Nethead001/menu/main/.1.13.zip"
+yes A | unzip m.zip
+rm -fr m.zip
+chmod +x *
+cd /usr/bin/rere
+chmod +x *
+cd
+#Apache2 Fix
+systemctl daemon-reload
+systemctl stop apache2
+systemctl disable apache2
+Seting Stunnel4
+apt install stunnel4 -y
+cat > /etc/stunnel/stunnel.conf <<-END
+cert = /etc/xray/funny.pem
+client = no
+socket = a:SO_REUSEADDR=1
+socket = l:TCP_NODELAY=1
+socket = r:TCP_NODELAY=1
+[openssh]
+accept = 777
+connect = 127.0.0.1:3303
+[dropbear]
+accept = 447
+connect = 127.0.0.1:109
+[openvpn]
+accept = 444
+connect = 127.0.0.1:1194
+END
+#Set Timezone GMT+7
+timedatectl set-timezone Asia/Jakarta;
+#install latest xray
+cd /usr/bin
+wget -q https://raw.githubusercontent.com/Nethead001/sslh/main/xray.zip
+yes A | unzip xray.zip
+rm -fr xray.zip
+chmod +x *.dat
+chmod +x xray
+cd
+#profile
+apt install neofetch -y
+cat > /root/.profile << END
+if [ "$`BASH" ]; then
+if [ -f ~/.bashrc ]; then
+. ~/.bashrc
+fi
+fi
+mesg n || true
+clear
+#neofetch --ascii_distro Arcolinux_small
+info
+END
+chmod 644 /root/.profile
+#crontab
+echo "0 0,6,12,18 * * * root backup
+0,15,30,45 * * * * root /usr/bin/xp
+*/5 * * * * root limit-ip
+*/30 * * * * root clear-log
+*/5 * * * * root kill-quota" >> /etc/crontab
+systemctl restart cron
+#Install VNSTAT
+apt install vnstat -y
+Menginstal Argo Core
+wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
+sudo dpkg -i cloudflared-linux-amd64.deb
+rm -f cloudflared-linux-amd64.deb
+Menginstall Nodejs
+apt install nodejs -y
+#Install Speedtest
+#curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
+#sudo apt-get install speedtest -y
+#install nginx
+apt install nginx -y
+rm /etc/nginx/conf.d/default.conf
+cd /etc/nginx
+wget -q -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/Nethead001/sslh/main/nginx.conf"
+cd
+#detail nama perusahaan
+country="PK"
+state="Sindh"
+locality="Kab. Kota Waringin Timur"
+organization="Cyber Project"
+organizationalunit="99999"
+commonname="Cyber"
+email="mrshahnawazyt5@gmaul.com"
+delete
+systemctl stop nginx haproxy
+rm -f /etc/xray/xray*
+rm -f /etc/xray/funny.pem
+make a certificate
+openssl genrsa -out /etc/xray/xray.key 2048
+openssl req -new -x509 -key /etc/xray/xray.key -out /etc/xray/xray.crt -days 1095 
+ -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=`$email"
+cat /etc/xray/xray.key /etc/xray/xray.crt > /etc/xray/funny.pem
+chmod 644 /etc/xray/*
+systemctl restart nginx haproxy
+#install socat
+apt install iptables -y
+apt install curl socat xz-utils wget apt-transport-https gnupg gnupg2 gnupg1 dnsutils lsb-release -y
+apt install socat cron bash-completion -y
+#install firewall
+apt install ufw -y
+swap ram 2gb
+wget https://raw.githubusercontent.com/Cretezy/Swap/master/swap.sh -O swap
+sh swap 2G
+rm swap
+Make Other
+touch /etc/xray/proxy.log
+Install NoobzVPN'S
+[ Create Directory File ]
+mkdir -p /etc/noobzvpns
+[ Membersihkan layar ]
+clear
+[ Membuat Json Config yang di gunakan pada server ]
+cat > /etc/noobzvpns/config.json <<-JSON
+{
+"tcp_std": [ 8080 ],
+"tcp_ssl": [ 8443 ],
+"ssl_cert": "/etc/noobzvpns/cert.pem",
+"ssl_key": "/etc/noobzvpns/key.pem",
+"ssl_version": "AUTO",
+"conn_timeout": 60,
+"dns_resolver": "/etc/resolv.conf",
+"http_ok": "HTTP/1.1 101 Switching Protocols[crlf]Upgrade: websocket[crlf][crlf]"
+}
+JSON
+Port Dari tcp_std & tcp_ssl edit sesuai kemauan kalian agar tidak bentrok dengan service lain pada vps kalian
+[ wget ambil file ]
+wget -q -O /usr/bin/noobzvpns "https://github.com/noobz-id/noobzvpns/raw/master/noobzvpns.x86_64"
+wget -q -O /etc/noobzvpns/cert.pem "https://github.com/noobz-id/noobzvpns/raw/master/cert.pem"
+wget -q -O /etc/noobzvpns/key.pem "https://github.com/noobz-id/noobzvpns/raw/master/key.pem"
+[ memberi izin pada file json & cert + key ]
+chmod +x /etc/noobzvpns/*
+[ Memberi Izin Exec pada file biner ]
+chmod +x /usr/bin/noobzvpns
+[ Mengambil Service yang di perlukan ]
+wget -q -O /etc/systemd/system/noobzvpns.service "https://github.com/noobz-id/noobzvpns/raw/master/noobzvpns.service"
+[ Enable Service ]
+systemctl enable noobzvpns
+[ Firewall Port Noobz ]
+sudo iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 8443 -j ACCEPT
+sudo ufw allow 8080/tcp
+sudo ufw allow 8443/tcp
+[ Restart Service ]
+systemctl restart noobzvpns
+[ Membersihkan layar ]
+clear
+Install Slowdns
+cd
+wget -q https://raw.githubusercontent.com/Nethead001/Autoscript/main/slowdns.sh; chmod +x slowdns.sh; ./slowdns.sh; rm -fr slowdns.sh
+Install Chisel Proxy
+wget -q https://raw.githubusercontent.com/Nethead001/Autoscript/main/chisel.sh; chmod +x chisel.sh; ./chisel.sh; rm -fr chisel.sh
+#Install Lainya
+wget -q https://github.com/Rerechan02/UDP/main/udp.sh && chmod +x udp.sh && ./udp.sh
+Install Plugin HideSSH
+wget -q https://github.com/praiman99/Plugin-FN/raw/Beginner/plugin.sh ; chmod 777 plugin.sh ; ./plugin.sh ; rm -fr plugin.sh
+Install Warp Cloudflare
+cd /root; wget -O wgcf.sh "https://raw.githubusercontent.com/Nethead001/warp/main/install-warp.sh"; chmod +x /root/; /root/wgcf.sh; rm -fr /root/
+Install Backup Google Drive
+cd /root
+wget https://raw.githubusercontent.com/Nethead001/enhanced/main/set-br.sh
+chmod +x /root/*
+./set-br.sh
+rm -fr /root/*
+clear
+#Install OpenVPN
+cd /usr/bin
+wget -q https://raw.githubusercontent.com/Nethead001/Nethead001/main/openvpn/vpn.sh && chmod +x vpn.sh && ./vpn.sh
+// Membuat Service
+cat> /etc/systemd/system/xray.service << END
+[Unit]
+Description=Xray by FunnyVPN
+Documentation=https://indo-ssh.com
+After=network.target nss-lookup.target
+[Service]
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/xray -config /etc/xray/config.json
+Restart=on-failure
+RestartPreventExitStatus=23
+[Install]
+WantedBy=multi-user.target
+END
